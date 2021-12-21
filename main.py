5ce5dc85d8e6b7183df36ee2964d202983f94c42
Erlantz <96289199+GitPushErlantz@users.noreply.github.com>
2021-12-21 19:12:19 +0100

    Date

--- main.py
+++ main.py
@@ -1,19 +1,27 @@
 # CREATE A MENU WITH DIFFERENT OPTIONS THAT WILL BE ABLE TO WORK WITH STRINGS
 import os
 import datetime
-
 from functions import *
 
+now = datetime.datetime.now()
+CurrentDate = "%d/%m/%Y"
+CurrentDateStr = "%a, %b %d"
+CurrentTime = "%H:%M"
+CurrentTimeSec = "%H:%M:%S"
+print(now.strftime(CurrentDate))
+print(now.strftime(CurrentDateStr))
+print(now.strftime(CurrentTime))
+print(now.strftime(CurrentTimeSec))
 #____________________________________________________________________________MENU
 aukera = -1
 while aukera != 0:
-    print("Hello, %s! It is now" %os.getlogin())
+    print(f"Hello, {os.getlogin()}! It is now {now.strftime(CurrentTime)} of {now.strftime(CurrentDateStr)}.")
     print("------------------------MENU------------------------")
     print("1) Stopwatch") #Count the time something takes
     print("2) Alarm") #Alarm clock that will ring at a determined hour
     print("3) Pomodoro") #automatic timer to work/rest
     print("4) To-do list")  #view or edit to-do list
-    print("5) Calendar")
+    print("5) Calendar") #print current month or let the user choose
     print("0) Exit.")
     aukera = int(input())
     #if aukera == 1:
