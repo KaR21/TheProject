ed25774a634632428d11eefb8856922e8e61c05c
Erlantz <96289199+GitPushErlantz@users.noreply.github.com>
2021-12-21 12:28:02 +0100

    improve the wording, say hello to the user
    gets windows username and says hello, will add the datetime in the next version

--- main.py
+++ main.py
@@ -1,25 +1,28 @@
 # CREATE A MENU WITH DIFFERENT OPTIONS THAT WILL BE ABLE TO WORK WITH STRINGS
+import os
+import datetime
 
 from functions import *
 
 #____________________________________________________________________________MENU
 aukera = -1
 while aukera != 0:
-
+    print("Hello, %s! It is now" %os.getlogin())
     print("------------------------MENU------------------------")
-    print("1) Chronometer")
-    print("2) Alarm")
-    print("3) Pomodoro")
-    print("4) Print TODO List")  #view and if the user wants add to to do list
+    print("1) Stopwatch") #Count the time something takes
+    print("2) Alarm") #Alarm clock that will ring at a determined hour
+    print("3) Pomodoro") #automatic timer to work/rest
+    print("4) To-do list")  #view or edit to-do list
+    print("5) Calendar")
     print("0) Exit.")
     aukera = int(input())
-    if aukera == 1:
+    #if aukera == 1:
 
-    elif aukera == 2:
+    #elif aukera == 2:
 
-    elif aukera == 3:
+    #elif aukera == 3:
 
-    elif aukera == 4:
+    #elif aukera == 4:
 
-print("Good bye!")
+print("Goodbye!")
 #____________________________________________________________________________MENU
