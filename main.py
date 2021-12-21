279e36e475ec3645d980482cd1a98be00d2abbdd
KaR21 <96118700+KaR21@users.noreply.github.com>
2021-12-21 12:08:52 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,25 @@
+# CREATE A MENU WITH DIFFERENT OPTIONS THAT WILL BE ABLE TO WORK WITH STRINGS
+
+from functions import *
+
+#____________________________________________________________________________MENU
+aukera = -1
+while aukera != 0:
+
+    print("------------------------MENU------------------------")
+    print("1) Chronometer")
+    print("2) Alarm")
+    print("3) Pomodoro")
+    print("4) Print TODO List")  #view and if the user wants add to to do list
+    print("0) Exit.")
+    aukera = int(input())
+    if aukera == 1:
+
+    elif aukera == 2:
+
+    elif aukera == 3:
+
+    elif aukera == 4:
+
+print("Good bye!")
+#____________________________________________________________________________MENU
