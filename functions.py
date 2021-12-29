c1256ab7372f5a7b87e2ba604c1c8eeed1ed33d2
Erlantz <96289199+GitPushErlantz@users.noreply.github.com>
2021-12-29 16:08:19 +0100

    Read & Write
    - Read and write save data in a data.txt file
    - Created functions for features
    - PENDING load function (doesn't work yet)

--- functions.py
+++ functions.py
@@ -1,1 +1,45 @@
-#hello fucking world
+import os
+import linecache
+
+
+def create():
+    with open("data.txt", "w", encoding="utf-8") as f:  # opens data.txt in write mode to edit settings
+        f.writelines(f"{os.getlogin()}\n")  # defaults name to the windows username
+        f.writelines("1\n")  # defaults the date format to DD/MM/YYYY
+        f.writelines("False\n")  # defaults clock to not to show seconds
+        f.writelines("1\n")  # defaults theme to classic
+        f.writelines("False")  # defaults to not clearing the terminal after switching menus
+
+
+def load(name, preferredDateFormat, withSeconds, userTheme, keepItClean):
+    with open("data.txt", "r") as dataFile:  # these variables are stored locally, this opens data.txt in read mode
+        name = linecache.getline('data.txt', 1).replace("", "")[:-1]  # username, defaults to windows username
+        preferredDateFormat = linecache.getline('data.txt', 2).replace("", "")[:-1]  # the user's date format choice
+        withSeconds = linecache.getline('data.txt', 3).replace("", "")[:-1]  # decides if the clock will show seconds
+        userTheme = linecache.getline('data.txt', 4).replace("", "")[:-1]  # decides the color scheme
+        keepItClean = linecache.getline('data.txt', 5)  # decides if lines will be cleared after returning to main menu
+
+
+
+def stopwatch():
+    print("---------------------STOPWATCH---------------------")
+
+
+def alarm():
+    print("-----------------------ALARM-----------------------")
+
+
+def pomodoro():
+    print("----------------------POMODORO----------------------")
+
+
+def todo():
+    print("---------------------TO DO LIST---------------------")
+
+
+def calendar():
+    print("----------------------CALENDAR----------------------")
+
+
+def settings():
+    print("----------------------SETTINGS----------------------")
--- main.py
+++ main.py
@@ -1,49 +1,58 @@
 #   CREATE A MENU WITH DIFFERENT OPTIONS THAT WILL BE ABLE TO WORK WITH STRINGS
+import linecache
 import os
 import datetime
 from functions import *
 
-initialized = False  # will be stored locally for the first time setup (NOT DONE OR PLANNED YET)
-name = os.getlogin()  # get the username from the Windows user. Modifiable by the user itself
-preferredDateFormat = 1  # will decide the user's date choice, we will attempt to store this locally
-userTheme = 1  # will decide the color scheme, we will attempt to store this locally
+#  ___________________________________________________________________________SAVE AND LOAD DATA
+if not(os.path.isfile("data.txt")):   # if data.txt does not exist
+    create()   # creates save file
+
+
+with open("data.txt", "r") as dataFile:  # these variables are stored locally, this opens data.txt in read mode
+    name = linecache.getline('data.txt', 1).replace("", "")[:-1]  # username, defaults to windows username
+    preferredDateFormat = linecache.getline('data.txt', 2).replace("", "")[:-1]  # the user's date format choice
+    withSeconds = linecache.getline('data.txt', 3).replace("", "")[:-1]  # decides if the clock will show seconds
+    userTheme = linecache.getline('data.txt', 4).replace("", "")[:-1]  # decides the color scheme
+    keepItClean = linecache.getline('data.txt', 5)  # decides if lines will be cleared after returning to main menu
+
+load(name, preferredDateFormat, withSeconds, userTheme, keepItClean)
+
+#  ____________________________________________________________________________DATE AND TIME VARIABLES
 now = datetime.datetime.now()  # gets the current date
 CurrentDate = "%d/%m/%Y"  # formats the current date as 31/12/2000
 CurrentDateStr = "%a, %b %d"  # formats the current date as Sun, Dec 31
 CurrentTime = "%H:%M"  # formats the current time as 13:30
 CurrentTimeSec = "%H:%M:%S"  # formats the current time as 13:30:00
+
 #  ____________________________________________________________________________MENU
 menuOption = -1
 while menuOption != 0:
     print(f"Hello, {name}! It is now {now.strftime(CurrentTime)} of {now.strftime(CurrentDateStr)}.")
     print("------------------------MENU------------------------")
-    print("1) Stopwatch")  # Count the time something takes
-    print("2) Alarm")  # Alarm clock that will ring at a determined hour
+    print("1) Stopwatch")  # count the time something takes
+    print("2) Alarm")  # alarm clock that will ring at a determined hour
     print("3) Pomodoro")  # automatic timer to work/rest
     print("4) To-do list")  # view or edit to-do list
     print("5) Calendar")  # print current month or let the user choose
-    print(
-        "9) Settings")  # (will probably not be added) change a few settings like date format preference, name or theme
+    print("9) Settings")  # change a few settings like date format preference, name or theme
     print("0) EXIT")
     print("----------------------------------------------------")
     menuOption = int(input("Choose an option: "))
 
-    #  if menuOption == 1:
-
-    #  elif menuOption == 2:
-
-    #  elif menuOption == 3:
-
-    #  elif menuOption == 4:
-
-    #  elif menuOption == 5:
-
-    #  elif menuOption == 9:
-
-    #  elif menuOption == 0:
-
-    #  else:
-    #  print("invalid number")
-
-print("Goodbye!")
-#  ____________________________________________________________________________MENU
+    if menuOption == 1:
+        stopwatch()
+    elif menuOption == 2:
+        alarm()
+    elif menuOption == 3:
+        pomodoro()
+    elif menuOption == 4:
+        todo()
+    elif menuOption == 5:
+        calendar()
+    elif menuOption == 9:
+        settings()
+    elif menuOption == 0:
+        print("Goodbye!")
+    else:
+        print("invalid number")
