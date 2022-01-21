fcf77659a3dcdb1092c022c08d45d9999ae03f60
Erlantz <96289199+GitPushErlantz@users.noreply.github.com>
2022-01-21 11:51:03 +0100

    Markel's part
    Stopwatch + Calendar

--- functions.py
+++ functions.py
@@ -1,6 +1,8 @@
 import os
 import linecache
 import time
+import datetime
+import calendar
 
 from colorama import Fore, Back, Style
 from pygame import mixer
@@ -85,7 +87,28 @@ def stopwatch():
 
 def alarm():
     print("-----------------------ALARM------------------------")
+    timeNow = datetime.datetime.now()
+        currentDate = timeNow.strftime('%d/%m/%y %H:%M')
+        print(f'Current Date and Time is : {currentDate}')
 
+        userInputDate = 'None'
+        alarmDate = ''
+
+        while userInputDate not in ('Y', 'N'):
+            userInputDate = input('Want to set alarm on same date?(y/n): ').upper()
+            if userInputDate == 'Y':
+                userInput = input('Please Enter alarm time in %H:%M : ')
+                alarmDate = timeNow.strftime('%d/%m/%y') + ' ' + userInput
+                print(f'Alarm is set to : {alarmDate}')
+            else:
+                userInput = input('Please Enter alarm time in %d/%m/%y %H:%M : ')
+                alarmDate = userInput
+                print(f'Alarm is set to : {alarmDate}')
+           while True:
+            if datetime.datetime.now().strftime('%d/%m/%y %H:%M') == alarmDate:
+                mixer.music.load("./aud/work.mp3")
+                mixer.music.play()
+                break
 
 def pomodoro():
     mixer.init()
