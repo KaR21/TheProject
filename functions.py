a5ae731ccafb5155487ce78868af6167a624fa9c
markelrodri1 <96288766+markelrodri1@users.noreply.github.com>
2022-01-21 10:32:20 +0100

    Add files via upload

--- functions.py
+++ functions.py
@@ -1,3 +1,56 @@
+<<<<<<< HEAD
+import datetime
+
+from pygame import mixer
+
+
+def alarm():
+    print("-----------------------ALARM------------------------")
+    mixer.init()
+    timeNow = datetime.datetime.now()
+    currentDate = timeNow.strftime('%d/%m/%y %H:%M')
+    print(f'Current Date and Time is : {currentDate}')
+
+    userInputDate = 'None'
+    alarmDate = ''
+
+    while userInputDate not in ('Y', 'N'):
+        userInputDate = input('Want to set alarm on same date?(y/n): ').upper()
+        if userInputDate == 'Y':
+            userInput = input('Please Enter alarm time in %H:%M : ')
+            alarmDate = timeNow.strftime('%d/%m/%y') + ' ' + userInput
+            print(f'Alarm is set to : {alarmDate}')
+        else:
+            userInput = input('Please Enter alarm time in %d/%m/%y %H:%M : ')
+            alarmDate = userInput
+            print(f'Alarm is set to : {alarmDate}')
+
+    while True:
+        if datetime.datetime.now().strftime('%d/%m/%y %H:%M') == alarmDate:
+            mixer.music.load("./aud/work.mp3")
+            mixer.music.play()
+            break
+
+
+def calendar():
+    print("----------------------CALENDAR----------------------")
+    import calendar
+    userInputDate = 'None'
+    currentDateTime = datetime.datetime.now()
+
+    while userInputDate not in ('Y', 'N'):
+        userInputDate = input('do you want to see the calendar of this month?(y/n): ').upper()
+        if userInputDate == 'Y':
+
+            print(calendar.month(currentDateTime.year, currentDateTime.month))
+        else:
+            year = int(input("Input the year : "))
+            month = int(input("Input the month : "))
+
+            print(calendar.month(year, month))
+
+
+=======
 import datetime
 import time
 
@@ -54,3 +107,4 @@ def calendar():
             print(calendar.month(year, month))
 
 
+>>>>>>> c5888464865aa7737eb4a019918368e255b0de53
