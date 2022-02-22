c9424952302052b37f6f1c1633b486c4e2b64601
Karmele Elorza <96118700+KaR21@users.noreply.github.com>
2022-02-22 11:56:57 +0100

    Update functionsCSV.py

--- functionsCSV.py
+++ functionsCSV.py
@@ -1,20 +1,23 @@
 import csv
-f = open("names.csv", 'w')
-with f:
-    fnames = ('first_name', 'last_name')
-    writer = csv.DictWriter(f, fieldnames=fnames)
-    writer.writeheader()
-    a = 'y'
-    while a=='y':
-        fn = input("Enter a first name")
-        sn = input("Enter a last name")
-        writer.writerow({'first_name':fn, 'last_name': sn})
-        a = input("Do you want to add another people? y/n")
 
-f = open("names.csv", 'r')
-with f:
-    reader = csv.DictReader(f)
-    headers = reader.fieldnames
-    print(headers)
-    for row in reader:
-        print(row['first_name'], row['last_name'])
+def csvManagement():
+    f = open("names.csv", 'w')
+    with f:
+        fnames = ('first_name', 'last_name')
+        writer = csv.DictWriter(f, fieldnames=fnames)
+        writer.writeheader()
+        a = 'y'
+        while a == 'y':
+            fn = input("Enter a first name")
+            sn = input("Enter a last name")
+            writer.writerow({'first_name': fn, 'last_name': sn})
+            a = input("Do you want to add another people? y/n")
+
+    f = open("names.csv", 'r')
+    with f:
+        reader = csv.DictReader(f)
+        headers = reader.fieldnames
+        print(headers)
+        for row in reader:
+            print(row['first_name'], row['last_name'])
+
